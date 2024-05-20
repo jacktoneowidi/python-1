e28c39e2da85d606ca8093a4d8c6da002e00eae5
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2024-05-20 09:18:42 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
 name = input('whats your name ?')
 print ('hello', name)
 
+#def hello():
+   # print('hello')
+
+#name = input('whats your name \n')
+#hello()
+#print(name)
+
+name = input('whats your name ?')
+print ('hello', name)
+
 def hello():
     print('hello')
 
