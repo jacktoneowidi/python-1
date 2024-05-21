f89bcada14df78069be0876b628ad76a8664bad8
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2024-05-21 09:46:37 -0700

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,21 @@
-print('welcome to tuffgon one time hotel')
-print('this hotel would like to know your name')
+print('welcome to tuffgone one time hotel\n')
 
-name = input('whats your name')
-print (name, 'thank you for passing by')
+print('one time hotel would like to know your name\n')
+
+name = input('whats your name ?\n')
+print(name, 'thank you for passing by \n')
+print ('we have various delicious food on our menu', name)
+print (name,'here is our menu\n')
+menu = 'chips, chicken, pizza, juice, greens\n'
+print(menu)
+
+print('you can place your order')
+order = input()
+print (name , 'your,', order, 'order has been recived and it will be presented in a few minutes')
+
+
+
+#simple tuffgone hotel order system
+
 
 
