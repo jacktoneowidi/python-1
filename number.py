31b995040bb0bf2c8b9da7f28099854b9eb282b0
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2024-05-22 09:40:49 -0700

    intergrating numbers in python

--- number.py
+++ number.py
@@ -1,12 +1,31 @@
-name = 'jacktone owidi'
-age = 23
+#name = 'jacktone owidi'
+#age = 23
 
-print (name)
-print (age)
-print(type (name))
-print(type(age))
+#print (name)
+#print (age)
+#print(type (name))
+#print(type(age))
 
 
 
-actual_age = 23.7
-print(type(actual_age ))
+#actual_age = 23.7
+#print(type(actual_age ))
+print ('welcome to tuff resturant\n')
+print('tuff resturant would like to know your name\n')
+name = input('whats your name\n')
+print (name, 'thanks for stoppign by. Here is our menu,\n' )
+menu = 'chips, chicken, salad, pizza, juice\n'
+print(menu)
+print(name,'what would you like to order')
+order = input()
+
+
+price = 1
+quantity = input('how many packets of would you like\n')
+
+total = price * quantity
+print (total)
+print(name,'your order has been recived and it will presented soon')
+
+
+
