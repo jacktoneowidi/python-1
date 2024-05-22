2adf1e5cdf7774829ffc5df42d2cff49db94d66e
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2024-05-22 05:51:53 -0700

    Update variables.py

--- variables.py
+++ variables.py
@@ -17,5 +17,16 @@ print (name , 'your,', order, 'order has been recived and it will be presented i
 
 #simple tuffgone hotel order system
 
+# recap  
+print('welcome to one time tuff hotel\n')
+print('thank you for passing by. we would like to know your name')
+
+name = input('whats your name ?\n')
+print(name,'thank you for stopping by, Here is our menu\n')
+menu = 'chicken, fish, chips, juice.'
+print (name,'what would you like from our menue ', menu)
+order = input()
+print(name, 'nice choice your ',order, 'has been recived and it wount take long')
+
 
 
